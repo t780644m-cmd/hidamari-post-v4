a88a32f21b1ac84618d1a3f18c0aec30e41bc693
t780644m-cmd <t780644m@kago.ed.jp>
2026-07-08 12:46:10 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "ここにあなたのAPIキー",
-  authDomain: "ここにあなたのauthDomain",
-  projectId: "ここにあなたのprojectId",
-  storageBucket: "ここにあなたのstorageBucket",
-  messagingSenderId: "ここにあなたのmessagingSenderId",
-  appId: "ここにあなたのappId"
+  apiKey: "AIzaSyB0kyn1IVAPBLNP-uGxpjllPyiPCJunGII",
+  authDomain: "hidamari-post-22d4e.firebaseapp.com",
+  projectId: "hidamari-post-22d4e",
+  storageBucket: "hidamari-post-22d4e.firebasestorage.app",
+  messagingSenderId: "960419389881",
+  appId: "1:960419389881:web:be2f7f3cb068ad75b2d668"
 };
 
 const app = initializeApp(firebaseConfig);

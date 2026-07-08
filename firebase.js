cb9f5f97a7a141bc860c162ac1bc04be978b16e7
t780644m-cmd <t780644m@kago.ed.jp>
2026-07-08 12:44:39 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,16 @@
-// Import the functions you need...
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  ...
+  apiKey: "ここにあなたのAPIキー",
+  authDomain: "ここにあなたのauthDomain",
+  projectId: "ここにあなたのprojectId",
+  storageBucket: "ここにあなたのstorageBucket",
+  messagingSenderId: "ここにあなたのmessagingSenderId",
+  appId: "ここにあなたのappId"
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
